b29f7326d792688c625aa0aa6a3f07951b393efb
Vladislav Medvedev <medvedev_vv@bw-sw.com>
2017-09-27 14:16:32 +0700

    CSVS-17 Remarks were fixed

--- src/main/scala/com/bwsw/cloudstack/vault/server/kafka/Consumer.scala
+++ src/main/scala/com/bwsw/cloudstack/vault/server/kafka/Consumer.scala
@@ -21,6 +21,7 @@ package com.bwsw.cloudstack.vault.server.kafka
 import java.util.concurrent.CountDownLatch
 import java.util.{Collections, Properties}
 
+import com.bwsw.cloudstack.vault.server.common.InterruptableCountDawnLatch
 import com.bwsw.cloudstack.vault.server.common.traits.EventHandler
 import com.bwsw.cloudstack.vault.server.util.exception.CriticalException
 import org.apache.kafka.clients.consumer.{ConsumerConfig, KafkaConsumer}
@@ -28,7 +29,7 @@ import org.slf4j.LoggerFactory
 
 import scala.collection.JavaConverters._
 import scala.concurrent.{ExecutionContext, Future}
-import scala.util.{Failure, Success}
+import scala.util.{Failure, Success, Try}
 
 /**
   * Class is responsible for getting events from Kafka topic
@@ -70,7 +71,7 @@ class Consumer[T](val brokers: String,
     val records = consumer.poll(pollTimeout)
 
     val futureEvents: Set[(Future[Unit], T)] = eventHandler.handleEventsFromRecords(records.asScala.map(_.value()).toList)
-    val eventLatch: CountDownLatch  = new CountDownLatch(futureEvents.size)
+    val eventLatch: InterruptableCountDawnLatch = new InterruptableCountDawnLatch(new CountDownLatch(futureEvents.size))
 
     futureEvents.foreach { x =>
       checkEvent(x)
@@ -86,20 +87,22 @@ class Consumer[T](val brokers: String,
           future.onComplete {
             case Success(x) =>
               logger.info(s"The event: $event was successful")
-              eventLatch.countDown()
+              eventLatch.succeed()
             case Failure(e: CriticalException) =>
               logger.error(s"An exception occurred during the event: $event processing: $e")
               if (eventHandler.isNonFatalException(e)) {
                 logger.warn("The exception: \"" + s"${e.exception}" + "\"" +
                   s"which was thrown while event: $event was being handle, is not fatal and will be ignored")
-                eventLatch.countDown()
+                eventLatch.succeed()
               } else {
                 logger.warn("The exception: \"" + s"${e.exception}" + "\"" +
                   s"is fatal, the event: $event will be restarted")
                 val restartedEvent = eventHandler.restartEvent(event)
                 checkEvent(restartedEvent)
               }
-            case Failure(e: Throwable) => throw e
+            case Failure(e: Throwable) =>
+              logger.error(s"Unhandled exception was thrown: $e")
+              eventLatch.abort()
           }
       }
     }
