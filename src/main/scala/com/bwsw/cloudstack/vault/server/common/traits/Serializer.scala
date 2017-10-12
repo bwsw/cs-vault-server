12d3d6ae26ed7215284c250236aaa53e2036338c
Vladislav Medvedev <medvedev_vv@bw-sw.com>
2017-10-12 17:57:11 +0700

    CSVS-44 Methods of JsonSerializer which have unclearly names were renamed

--- src/main/scala/com/bwsw/cloudstack/vault/server/common/traits/Serializer.scala
+++ src/main/scala/com/bwsw/cloudstack/vault/server/common/traits/Serializer.scala
@@ -21,7 +21,7 @@ package com.bwsw.cloudstack.vault.server.common.traits
 trait Serializer {
   def serialize(value: Any): String
   def deserialize[T: Manifest](value: String) : T
-  def setIgnoreUnknown(ignore: Boolean)
-  def getIgnoreUnknown() : Boolean
+  def setIgnoreUnknownPropertiesFlag(doesIgnoreUnknownProperties: Boolean)
+  def getIgnoreUnknownPropertiesFlag: Boolean
 }
 
